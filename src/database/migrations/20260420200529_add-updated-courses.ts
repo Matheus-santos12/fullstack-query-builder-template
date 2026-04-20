805ded089ef10d9564fc4fd9899be381db155a73
Matheus Silva <rematheussilva@gmail.com>
2026-04-20 15:16:51 -0500

    Criando migration para alterar tabela

---
+++ src/database/migrations/20260420200529_add-updated-courses.ts
@@ -0,0 +1,13 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("courses", (table) => {
+    table.timestamp("update_at").defaultTo(knex.fn.now()).after("create_ad");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("courses", (table) => {
+    table.dropColumn("update_at");
+  });
+}
