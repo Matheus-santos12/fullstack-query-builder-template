daee6b1f63f3f23f767074f34187b28e3a836c92
Matheus Silva <rematheussilva@gmail.com>
2026-04-20 15:45:09 -0500

    Revertendo migration

--- src/database/migrations/20260420200529_add-updated-courses.ts
+++ src/database/migrations/20260420200529_add-updated-courses.ts
@@ -2,7 +2,7 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable("courses", (table) => {
-    table.timestamp("update_at").defaultTo(knex.fn.now()).after("create_ad");
+    table.timestamp("update_at").defaultTo(knex.fn.now()).after("create_at");
   });
 }
 
